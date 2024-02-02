911857a1dd54d052f5b3842feb2ef605499e299b
Caribla <caribla4@gmail.com>
2024-02-02 13:30:34 +0100

    Power and channel buttons basic functionality to change screen channel

--- js/television.js
+++ js/television.js
@@ -1,1 +1,27 @@
-console.log("I'm loaded and ready to change the channels");
+// Getting the buttons from the controller, first the channelButtons and after that the power button
+const channelButtons = document.getElementsByClassName("channelButtons");
+const onOffButton = document.getElementById("powerButton");
+
+// The buttonChannel HTMLCollection, converted into array
+let arraychannelButtons = Array.from(channelButtons);
+
+// Saving screen HTML element on a variable
+const screen = document.getElementById("televisionScreen");
+
+// Power button OnClickListener function 
+onOffButton.addEventListener("click", () => {
+    screen.classList.remove(screen.classList[screen.classList.length - 1]);
+    let channelClass = "screenOff";
+    screen.classList.add(channelClass);
+})
+
+// Mapping all the channel buttons and setting OnClickListener to change between channels
+arraychannelButtons.map(
+    item => {
+        item.addEventListener("click", (e) => {
+            screen.classList.remove(screen.classList[screen.classList.length - 1]);
+            let channelClass = "channel"+e.target.id;
+            screen.classList.add(channelClass);
+        })
+    }
+);
